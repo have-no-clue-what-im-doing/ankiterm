02196301ed23c43cdaa3160ed865e45a203b37c6
Broderic Duncan <brodericjduncan@gmail.com>
2025-02-12 22:43:23 -0500

    back to basics

--- x/reviewer/streamrv/lib.go
+++ x/reviewer/streamrv/lib.go
@@ -2,12 +2,8 @@ package streamrv
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 	"strconv"
 	"strings"
-	"syscall"
-	"os/exec"
 
 	"github.com/pluveto/ankiterm/x/automata"
 	"github.com/pluveto/ankiterm/x/reviewer"
@@ -29,83 +25,68 @@ func Execute(am *automata.Automata, deck string) {
 	}
 	defer am.StopReview()
 
-	// Set up signal handling to exit cleanly on Ctrl+C
-	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGINT)
-
 	for {
-		select {
-		case <-signalChannel:
-			fmt.Println("\nExiting...")
+		// Clear the screen before displaying the next card
+		clearScreen()
+
+		card, err := am.NextCard()
+		if err != nil {
+			if strings.Contains(err.Error(), "Gui review is not currently active") {
+				fmt.Println("Congratulations! You have finished all cards.")
+				return
+			}
+			panic(err)
+		}
+		if err != nil {
+			fmt.Println("No more cards.")
 			return
+		}
+
+		fmt.Printf("\n[REVIEW MODE]\n")
+		fmt.Println(format(card.Question))
+		fmt.Println("\n[ENTER] Show Answer")
+
+		awaitEnter()
+		fmt.Print("\n---\n")
+		fmt.Println(format(card.Answer))
+
+		lookup := []string{"Again", "Hard", "Good", "Easy"}
+		for i, button := range card.Buttons {
+			fmt.Printf("[%d] %s (%s)\n", button, lookup[i], card.NextReviews[i])
+		}
+
+		action := awaitAction(am.CurrentCard().Buttons)
+		switch code := action.GetCode(); code {
+		case reviewer.ActionAbort:
+			return
+		case reviewer.ActionSkip:
+			continue
+		case reviewer.ActionAnswer:
+			am.AnswerCard(action.(reviewer.AnswerAction).CardEase)
 		default:
-			card, err := am.NextCard()
-			if err != nil {
-				if strings.Contains(err.Error(), "Gui review is not currently active") {
-					fmt.Println("Congratulations! You have finished all cards.")
-					return
-				}
-				panic(err)
-			}
-			if err != nil {
-				fmt.Println("No more cards.")
-				return
-			}
-
-			// Clear the screen after each card
-			clearScreen()
-
-			fmt.Printf("\n[REVIEW MODE]\n")
-			fmt.Println(format(card.Question))
-			fmt.Println("\n[Press any key to Show Answer]")
-			awaitEnter()
-
-			fmt.Print("\n---\n")
-			fmt.Println(format(card.Answer))
-
-			lookup := []string{"Again", "Hard", "Good", "Easy"}
-			for i, button := range card.Buttons {
-				fmt.Printf("[%d] %s (%s)\n", button, lookup[i], card.NextReviews[i])
-			}
-
-			action := awaitAction(am.CurrentCard().Buttons)
-			switch code := action.GetCode(); code {
-			case reviewer.ActionAbort:
-				return
-			case reviewer.ActionSkip:
-				continue
-			case reviewer.ActionAnswer:
-				am.AnswerCard(action.(reviewer.AnswerAction).CardEase)
-			default:
-				panic("unknown action code")
-			}
+			panic("unknown action code")
 		}
 	}
 }
 
-// Function to clear the terminal screen
 func clearScreen() {
-	cmd := exec.Command("clear") // for Unix/Linux
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+	fmt.Print("\033[H\033[2J")
 }
 
 func awaitEnter() {
-	// Wait for any key press to continue
 	var input string
-	fmt.Scanf("%s", &input)
+	fmt.Scanln(&input)
 }
 
 func awaitAction(validRange []int) reviewer.Action {
-	// No extra Enter key needed, input is captured directly
-	fmt.Print("Enter choice (1-4): ")
+	print("awaitAction")
 	var input string
-	fmt.Scanf("%s", &input)
+	fmt.Scanln(&input)
 
-	// Try to parse the input into an integer
+	// try parse int
 	i, err := strconv.Atoi(input)
 	if err != nil || !xslices.Contains(validRange, i) {
-		fmt.Printf("Invalid input \"%s\" out of range, try again: \n", input)
+		fmt.Printf("invalid input \"%s\" out of range, try again: \n", input)
 		return awaitAction(validRange)
 	}
 	return reviewer.ActionFromString(input)
